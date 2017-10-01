391e6c7012003d2863105b56a5d71c3266db2dfe
John Murphy <murphybytes@gmail.comn>
2017-10-01 17:03:32 -0500

    port env variable

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	log.Println("Starting goroku.....")
+	log.Println("Getting port")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalln("No port found")
+	}
+	log.Printf("Binding to port %s", port)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got request")
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
