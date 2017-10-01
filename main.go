bb3de029d9e3a03e305a956140262a15cd31ce99
John Murphy <murphybytes@gmail.comn>
2017-10-01 16:17:53 -0500

    added makefile

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 )
 
 func main() {
+	log.Println("Starting goroku.....")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request")
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
